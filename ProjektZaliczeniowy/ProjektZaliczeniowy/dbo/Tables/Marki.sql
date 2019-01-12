b8b109f420b6e8e3eacff774d315485d13bfb9a2
Nitril <athlonxp@poczta.fm>
2019-01-12 22:24:48 +0100

    Utworzenie bazy danych i tabeli marki

---
+++ ProjektZaliczeniowy/ProjektZaliczeniowy/dbo/Tables/Marki.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Marki] (
+    [Marka_Id]    SMALLINT      IDENTITY (1, 1) NOT NULL,
+    [Marka_Kod]   VARCHAR (10)  NOT NULL,
+    [Marka_Nazwa] VARCHAR (100) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Marka_Id] ASC),
+    UNIQUE NONCLUSTERED ([Marka_Kod] ASC)
+);
+
