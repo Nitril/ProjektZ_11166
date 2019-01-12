b09cb48c5215a449c02f803701b8ddfcec2d903e
Nitril <athlonxp@poczta.fm>
2019-01-12 22:30:00 +0100

    Stworzenie tabeli modele

---
+++ ProjektZaliczeniowy/ProjektZaliczeniowy/dbo/Tables/Modele.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Modele] (
+    [Model_Id]    SMALLINT      IDENTITY (1, 1) NOT NULL,
+    [Model_Kod]   VARCHAR (10)  NOT NULL,
+    [Model_Nazwa] VARCHAR (100) NOT NULL,
+    [Marka_Id]    SMALLINT      NOT NULL,
+    PRIMARY KEY CLUSTERED ([Model_Id] ASC),
+    CONSTRAINT [FK_Marka_Id] FOREIGN KEY ([Marka_Id]) REFERENCES [dbo].[Marki] ([Marka_Id]),
+    UNIQUE NONCLUSTERED ([Model_Kod] ASC)
+);
+
