f86acc1299bdfa24d738b719fef928104bc98689
Nitril <athlonxp@poczta.fm>
2019-01-13 18:16:05 +0100

    A: p 6

--- ProjektZaliczeniowy/ProjektZaliczeniowy/dbo/Tables/Zamowienia.sql
+++ ProjektZaliczeniowy/ProjektZaliczeniowy/dbo/Tables/Zamowienia.sql
@@ -12,11 +12,14 @@
     [Data_Sprzedazy]  SMALLDATETIME   NULL,
     [Data_Odbioru]    SMALLDATETIME   NULL,
     PRIMARY KEY CLUSTERED ([Zamowienie_Id] ASC),
+    CONSTRAINT [CHK_Cena_Sprzedazy] CHECK ([Cena_Sprzedazy]>(0)),
     CONSTRAINT [FK_Klient_Id_Zamowienia] FOREIGN KEY ([Klient_Id]) REFERENCES [dbo].[Klient] ([Klient_Id]) ON DELETE SET DEFAULT ON UPDATE SET DEFAULT,
     CONSTRAINT [FK_Mod_Id_Zamowienia] FOREIGN KEY ([Mod_Id]) REFERENCES [dbo].[Modele] ([Mod_Id]) ON DELETE SET DEFAULT ON UPDATE SET DEFAULT,
     CONSTRAINT [FK_Oferta_Id_Zamowienia] FOREIGN KEY ([Oferta_Id]) REFERENCES [dbo].[Cennik] ([Oferta_Id]),
     CONSTRAINT [FK_Sprzedawca_Id_Zamowienia] FOREIGN KEY ([Sprzedawca_Id]) REFERENCES [dbo].[Pracownik] ([Pracownik_Id]) ON DELETE SET DEFAULT ON UPDATE SET DEFAULT
 );
+
+
 
 
 
