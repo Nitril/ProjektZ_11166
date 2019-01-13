f178926d1f492ee3ab61fd63c03cec26fcd93ab6
Nitril <athlonxp@poczta.fm>
2019-01-13 17:51:17 +0100

    Część A pkt 3

--- ProjektZaliczeniowy/ProjektZaliczeniowy/dbo/Tables/Cennik.sql
+++ ProjektZaliczeniowy/ProjektZaliczeniowy/dbo/Tables/Cennik.sql
@@ -1,10 +1,12 @@
 ﻿CREATE TABLE [dbo].[Cennik] (
     [Oferta_Id] INT             IDENTITY (1, 1) NOT NULL,
-    [Mod_Id]    SMALLINT        NOT NULL,
+    [Mod_Id]    SMALLINT        CONSTRAINT [DF_Mod_Id_Cennik] DEFAULT ((-1)) NOT NULL,
     [Cena]      DECIMAL (10, 2) NOT NULL,
     [Oferta_Od] DATETIME        NOT NULL,
     [Oferta_Do] DATETIME        NULL,
     PRIMARY KEY CLUSTERED ([Oferta_Id] ASC),
-    CONSTRAINT [FK_Mod_Id_Cennik] FOREIGN KEY ([Mod_Id]) REFERENCES [dbo].[Modele] ([Mod_Id])
+    CONSTRAINT [FK_Mod_Id_Cennik] FOREIGN KEY ([Mod_Id]) REFERENCES [dbo].[Modele] ([Mod_Id]) ON DELETE SET DEFAULT ON UPDATE SET DEFAULT
 );
+
+
 
--- ProjektZaliczeniowy/ProjektZaliczeniowy/dbo/Tables/Zamowienia.sql
+++ ProjektZaliczeniowy/ProjektZaliczeniowy/dbo/Tables/Zamowienia.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[Zamowienia] (
     [Zamowienie_Id]   INT             IDENTITY (1, 1) NOT NULL,
-    [Klient_Id]       INT             NOT NULL,
-    [Sprzedawca_Id]   INT             NOT NULL,
-    [Mod_Id]          SMALLINT        NOT NULL,
-    [Oferta_Id]       INT             NOT NULL,
+    [Klient_Id]       INT             CONSTRAINT [DF_Klient_Id_Zamowienia] DEFAULT ((-1)) NOT NULL,
+    [Sprzedawca_Id]   INT             CONSTRAINT [DF_Sprzedawca_Id_Zamowienia] DEFAULT ((-1)) NOT NULL,
+    [Mod_Id]          SMALLINT        CONSTRAINT [DF_Mod_Id_Zamowienia] DEFAULT ((-1)) NOT NULL,
+    [Oferta_Id]       INT             CONSTRAINT [DF_Oferta_Id_Zamowienia] DEFAULT ((-1)) NOT NULL,
     [Ilosc]           INT             NOT NULL,
     [Zrealizowano]    BIT             NOT NULL,
     [Oplacono]        BIT             NOT NULL,
@@ -12,9 +12,11 @@
     [Data_Sprzedazy]  SMALLDATETIME   NULL,
     [Data_Odbioru]    SMALLDATETIME   NULL,
     PRIMARY KEY CLUSTERED ([Zamowienie_Id] ASC),
-    CONSTRAINT [FK_Klient_Id_Zamowienia] FOREIGN KEY ([Klient_Id]) REFERENCES [dbo].[Klient] ([Klient_Id]),
-    CONSTRAINT [FK_Mod_Id_Zamowienia] FOREIGN KEY ([Mod_Id]) REFERENCES [dbo].[Modele] ([Mod_Id]),
+    CONSTRAINT [FK_Klient_Id_Zamowienia] FOREIGN KEY ([Klient_Id]) REFERENCES [dbo].[Klient] ([Klient_Id]) ON DELETE SET DEFAULT ON UPDATE SET DEFAULT,
+    CONSTRAINT [FK_Mod_Id_Zamowienia] FOREIGN KEY ([Mod_Id]) REFERENCES [dbo].[Modele] ([Mod_Id]) ON DELETE SET DEFAULT ON UPDATE SET DEFAULT,
     CONSTRAINT [FK_Oferta_Id_Zamowienia] FOREIGN KEY ([Oferta_Id]) REFERENCES [dbo].[Cennik] ([Oferta_Id]),
-    CONSTRAINT [FK_Sprzedawca_Id_Zamowienia] FOREIGN KEY ([Sprzedawca_Id]) REFERENCES [dbo].[Pracownik] ([Pracownik_Id])
+    CONSTRAINT [FK_Sprzedawca_Id_Zamowienia] FOREIGN KEY ([Sprzedawca_Id]) REFERENCES [dbo].[Pracownik] ([Pracownik_Id]) ON DELETE SET DEFAULT ON UPDATE SET DEFAULT
 );
+
+
 
